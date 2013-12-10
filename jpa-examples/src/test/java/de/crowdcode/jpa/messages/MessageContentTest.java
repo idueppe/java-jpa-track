8468d730dcfddac27c778371b152bbbd7305758a
idueppe <ingo.dueppe@dueppe.com>
2013-12-10 16:51:04 +0100

    Add secondary table example

---
+++ jpa-examples/src/test/java/de/crowdcode/jpa/messages/MessageContentTest.java
@@ -0,0 +1,77 @@
+package de.crowdcode.jpa.messages;
+
+import static org.junit.Assert.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class MessageContentTest {
+	
+	private static EntityManagerFactory emf;
+	
+	private EntityManager em;
+	
+	private static Long messageId;
+	
+	@BeforeClass
+	public static void setUpClass()
+	{
+		emf = Persistence.createEntityManagerFactory("jpa-examples");
+	}
+	
+	@Before
+	public void setUp()
+	{
+		em = emf.createEntityManager();
+		em.getTransaction().begin();
+	}
+	
+	@After
+	public void tearDown()
+	{
+		if (em.getTransaction().isActive())
+			em.getTransaction().commit();
+		em.close();
+	}
+	
+	@AfterClass
+	public static void tearDownClass()
+	{
+		emf.close();
+	}
+
+	@Test
+	public void test_1_Persist() {
+		Message message = new Message();
+		message.setText("Hello Database");
+		message.setContent("Huge Content");
+		
+		em.persist(message);
+		assertNotNull(message.getId());
+		messageId = message.getId();
+	}
+	
+
+	@Test
+	public void test_2_Merge() {
+		Message message = new Message();
+		message.setId(messageId);
+		message.setText("Database");
+		message.setContent("Small Content");
+		
+		em.merge(message);
+		assertNotNull(message.getId());
+		messageId = message.getId();
+	}
+	
+}
