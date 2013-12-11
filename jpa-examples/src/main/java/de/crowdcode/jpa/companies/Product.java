447eeda45a91ad8321911375602bb92af79eeb96
idueppe <ingo.dueppe@dueppe.com>
2013-12-11 14:04:45 +0100

    Organized Imports

--- jpa-examples/src/main/java/de/crowdcode/jpa/companies/Product.java
+++ jpa-examples/src/main/java/de/crowdcode/jpa/companies/Product.java
@@ -3,7 +3,6 @@ package de.crowdcode.jpa.companies;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
