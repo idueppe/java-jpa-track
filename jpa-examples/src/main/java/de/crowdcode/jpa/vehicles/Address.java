3ebef09f989172fb42e200ebd7b70d9520ba1927
idueppe <ingo.dueppe@dueppe.com>
2013-12-11 16:02:50 +0100

    Added embedded example

---
+++ jpa-examples/src/main/java/de/crowdcode/jpa/vehicles/Address.java
@@ -0,0 +1,56 @@
+package de.crowdcode.jpa.vehicles;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+
+	private String street;
+	private String houseNo;
+	private String zip;
+	private String city;
+	
+	public Address() {
+	}
+	
+	public Address(String street, String houseNo, String zip, String city) {
+		super();
+		this.street = street;
+		this.houseNo = houseNo;
+		this.zip = zip;
+		this.city = city;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getHouseNo() {
+		return houseNo;
+	}
+
+	public void setHouseNo(String houseNo) {
+		this.houseNo = houseNo;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+}
